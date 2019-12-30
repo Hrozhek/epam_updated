ebe9c01595e66f722b208b8b282a6545bf0f036e
Hrozhek <hrozhek@gmail.com>
2019-12-30 12:55:43 +0300

    Update SimpleFileSaver.java

--- src/hometask12/common/solutions/files/SimpleFileSaver.java
+++ src/hometask12/common/solutions/files/SimpleFileSaver.java
@@ -35,24 +35,52 @@ public class SimpleFileSaver {
 
     public void save() {
         if (cargoService != null && carrierService != null && transportationService != null) {
-            saveCargoes();
-            saveCarriers();
-            saveTrasnportations();
+            try(FileWriter writer = new FileWriter(outputFile)) {
+                saveCargoes(writer);
+                saveCarriers(writer);
+                saveTrasnportations(writer);
+            }
         } else {
             System.out.println("Please set all services before call save() method");
         }
     }
 
-    public void saveCargoes() {
-        //write CargosBegin
+    public void saveCargoes(FileWriter writer) {
+        writer.write("CargosBegin\n");
         List<Cargo> cargoes = cargoService.getAll();
-        // for each make a line and then write
-        //write cargo end
+        for (Cargo cargo: cargoes) {
+            StringBuilder line = new StringBuilder();
+            List<Object> parametres = new ArrayList<>();
+            CargoType cargotype = cargo.getCargoType();
+            parameters.add(cargoType);
+            parameters.add(cargo.getId());
+            parameters.add(cargo.getName());
+            parameters.add(cargo.getWeight());
+            switch(cargoType):
+            case(FOOD) {
+                parameters.add((FoodCargo)cargo.getExpirationDate());
+                parameters.add((FoodCargo)cargo.getStoreTemperature());
+                break;
+            }
+            case(CLOTHES) {
+                parameters.add((ClothesCargo)cargo.getSize());
+                parameters.add((ClothesCargo)cargo.getMaterial())
+                break;
+            }
+            for (Object parameter: parameters) {
+                if (parameter != null) {
+                    line.append(parameter.toString());
+                }
+                line.append('|');
+            }
+            writer.write(line.toString() + "\n");
+        }
+        writer.write("CargosEnd\n");
     }
-    public void saveCarriers() {
+    public void saveCarriers(FileWriter writer) {
         List<Carrier> carriers = carrierService.getAll();
     }
-    public void saveTrasnportations() {
+    public void saveTrasnportations(FileWriter writer) {
         List<Transportation> transportations = transportationService.getAll();
     }
 }
